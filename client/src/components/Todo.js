d2aabf43019433f0af8efeee3628ad88d27cf115
Joe <josephc.lambert@gmail.com>
2019-10-09 14:43:11 -0700

    renders a note if the todo is overdue

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import { format } from 'date-fns';
+import { format, isAfter } from 'date-fns';
 
 const Todo = props => {
   const createdDate = format(props.createdAt, 'E MMM do, y');
   const completedDate = props.completedAt && format(props.completedAt, 'E MMM do, y');
   const formattedDueDate = props.dueDate && format(props.dueDate, 'E MMM do, y');
+  const overDue = props.dueDate && isAfter(new Date(), props.dueDate);
+
   return (
     <li>
       <div>
         <h3 className={`${props.completed ? '__completed' : ''}`}>
-          {props.item} {props.completedAt && <span>completed on {completedDate}</span>}
+          {props.item} {props.completedAt && <span>completed on {completedDate}</span>}{' '}
+          {props.dueDate && overDue && <span>This todo is over due!!!</span>}
         </h3>
         <p>Created on {createdDate}</p>
         {props.dueDate && <p>Due: {formattedDueDate}</p>}
+
         <div>
           <label htmlFor='todoItem'>Mark Completed</label>
           <input
