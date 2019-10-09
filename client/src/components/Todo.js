9ce57a4ff230f4ec01e1adea3353f16d2075a618
Joe <josephc.lambert@gmail.com>
2019-10-09 08:21:54 -0700

    moves date conversion into todo. adds class for completed todo. renders date todo was created.

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { format } from 'date-fns';
 
 const Todo = props => {
+  const date = format(props.createdAt, 'E MMM do, y');
   return (
     <li>
       <div>
-        <h3>
-          {props.item} {!props.completed && 'not'} completed
-        </h3>
-        <input type='checkbox' defaultChecked={props.completed} onClick={e => props.updateTodoStatus(e, props.id)} />
+        <h3 className={`${props.completed ? '__completed' : ''}`}>{props.item}</h3>
+        <p>Created on {date}</p>
+        <div>
+          <label htmlFor='todoItem'>Mark Completed</label>
+          <input
+            type='checkbox'
+            id='todoItem'
+            name='todoItem'
+            defaultChecked={props.completed}
+            onClick={e => props.updateTodoStatus(e, props.id)}
+          />
+        </div>
       </div>
     </li>
   );
